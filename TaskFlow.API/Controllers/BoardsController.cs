889617926b2e579434ef050eb3e5cecebd50f1ff
Sebastian K <sebastian.kili.dev@gmail.com>
2025-08-29 12:29:15 +0200

    feat(api): add v2 boards endpoint with pagination and api versioning

--- TaskFlow.API/Controllers/BoardsController.cs
+++ TaskFlow.API/Controllers/BoardsController.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +9,34 @@ namespace TaskFlow.API.Controllers;
 
 [Authorize]
 [ApiController]
-[Route("api/[controller]")]
+[ApiVersion("1.0")]
+[ApiVersion("2.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
 public class BoardsController(IMediator mediator) : ControllerBase
 {
+    // v1 endpoint
     [HttpGet]
+    [MapToApiVersion("1.0")]
     public async Task<IActionResult> GetBoards()
     {
         var boards = await mediator.Send(new GetBoardsQuery(User.GetUserId()));
         return Ok(boards);
+    }
+
+    // v2 endpoint with pagination
+    [HttpGet]
+    [MapToApiVersion("2.0")]
+    public async Task<IActionResult> GetBoardsV2([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+    {
+        var boards = await mediator.Send(new GetBoardsQuery(User.GetUserId()));
+        var pagedBoards = boards.Skip((page - 1) * pageSize).Take(pageSize);
+        return Ok(new
+        {
+            Page = page,
+            PageSize = pageSize,
+            TotalCount = boards.Count(),
+            Items = pagedBoards
+        });
     }
 
     [HttpGet("{id:guid}")]
